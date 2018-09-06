b67623fd84912c3669af65cad2efa9ae42fcd9ca
jayden.williams <jayden.williams@CLC-LAB209-17.TCSD.tooelesd.org>
2018-09-06 14:12:11 -0600

    chapter 2

---
+++ src/jayden_ch2_exe/Ch2_exe.java
@@ -0,0 +1,14 @@
+package jayden_ch2_exe;
+// Jayden williams
+// 9/18   Ch2 exe
+
+public class Ch2_exe {
+
+	public static void main(String[] args) {
+		// display some text
+		System.out.println("Welcome to Java");
+		System.out.println(2+2);
+
+	}
+
+}
