27bd5154854b9f5668cfd09b4555bee1e9c30bab
jayden.williams <jayden.williams@CLC-LAB209-17.TCSD.tooelesd.org>
2018-09-20 14:03:07 -0600

    ch2

--- src/jayden_ch2_exe/Ch2_exe.java
+++ src/jayden_ch2_exe/Ch2_exe.java
@@ -5,9 +5,32 @@ package jayden_ch2_exe;
 public class Ch2_exe {
 
 	public static void main(String[] args) {
-		// display some text
+		//display messages
 		System.out.println("Welcome to Java");
-		System.out.println(2+2);
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+		
+		
+		
+		System.out.println("Welcome to Java");
+		System.out.println("welcome to java");
+		System.out.println("welcome to java");
+		System.out.println("welcome to java");
+		System.out.println("welcome to java");
+		//quote
+		System.out.println("If moderation is a fault, then indifference is a crime.\r\n" + 
+				"Jack Kerouac");
+		//table
+		System.out.println("_______________________________________;\r\n" + 
+				"	|		a   |      a^     |      a^3   |;\r\n" + 
+				"	|1		    |1            |1           |;\r\n" + 
+				"	|2		    |4            |8           |;\r\n" + 
+				"	|3		    |9            |27          |;\r\n" + 
+				"	|4		    |16           |64          |;\r\n" + 
+				"	|		    |             |            |;\r\n" + 
+				"	|		    |             |            |;\r\n" + 
+				"	|		    |             |            |;\r\n" + 
+				"	---------------------------------------		");
 
 	}
 
